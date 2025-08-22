ad32dba2a6b5868a72221b67c91e96cb80587e35
Im-Siyoun <rlatldbs3321@sju.ac.kr>
2025-08-23 04:39:08 +0900

    feat(ai): implement AI chat functionality with chat history management

---
+++ apps/server/src/modules/ai/ai.controller.ts
@@ -0,0 +1,45 @@
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Post,
+  Sse,
+  UseGuards,
+} from '@nestjs/common';
+import { CurrentUser } from '../users/users.decorator';
+import { CreateTextRequestDto } from './ai.dto';
+import { AIService } from './ai.service';
+import { TokensGuard } from '../tokens/tokens.guard';
+
+@Controller('ai')
+export class AIController {
+  constructor(private readonly aiService: AIService) {}
+
+  @Post('chat/:id')
+  @Sse()
+  @UseGuards(TokensGuard)
+  async continueChat(
+    @CurrentUser() user,
+    @Body() { prompt }: CreateTextRequestDto,
+    @Param('id') chatId: string,
+  ) {
+    return await this.aiService.continueChat(user.name, chatId, prompt);
+  }
+
+  @Post('chat')
+  @Sse()
+  @UseGuards(TokensGuard)
+  async createChat(
+    @CurrentUser() user,
+    @Body() { prompt }: CreateTextRequestDto,
+  ) {
+    console.log(user);
+    return await this.aiService.startNewChat(user.name, prompt);
+  }
+
+  @Get('chat')
+  async getChat(@CurrentUser() user) {
+    return await this.aiService.getChattings(user?.name);
+  }
+}
--- apps/server/src/modules/ai/ai.service.ts
+++ apps/server/src/modules/ai/ai.service.ts
@@ -1,29 +1,175 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import OpenAI from 'openai';
+import { Observable } from 'rxjs';
+import { DbService } from '../db/db.service';
 
 @Injectable()
 export class AIService {
   private openai: OpenAI;
+  history: any[] = [];
 
-  constructor(private readonly configService: ConfigService) {
+  constructor(
+    private readonly configService: ConfigService,
+    private readonly db: DbService,
+  ) {
     this.openai = new OpenAI({
       apiKey: this.configService.get<string>('SOLAR_API_KEY'),
       baseURL: 'https://api.upstage.ai/v1',
     });
   }
 
-  async generateText(prompt: string): Promise<string> {
-    const chatChunks = await this.openai.chat.completions.create({
-      model: 'solar-pro2',
-      messages: [{ role: 'user', content: prompt }],
-      stream: true,
+  async continueChat(userName: string, chatId: string, prompt: string) {
+    const user = await this.db.user.findFirst({
+      where: { name: userName },
     });
-    let response = '';
-    for await (const chunk of chatChunks) {
-      response += chunk.choices[0]?.delta?.content || '';
+    if (!user) {
+      throw new UnauthorizedException('User not found');
+    }
+    const chat = await this.db.chat.findFirst({
+      where: { id: chatId, authorId: user.id },
+      include: { histories: true },
+    });
+    if (!chat) {
+      throw new UnauthorizedException('Chat not found');
     }
 
-    return response;
+    // TODO 여기부터 트랜잭션
+    await this.db.chat.update({
+      where: { id: chat.id },
+      data: {
+        histories: {
+          create: [
+            {
+              role: 'user',
+              content: prompt,
+            },
+          ],
+        },
+      },
+    });
+
+    const historyMessages = chat.histories.map((history) => ({
+      role: history.role as 'user' | 'assistant',
+      content: history.content,
+    }));
+    historyMessages.push({ role: 'user', content: prompt });
+
+    return new Observable((observer) => {
+      this.openai.chat.completions
+        .create({
+          model: 'solar-pro2',
+          messages: historyMessages,
+          stream: true,
+        })
+        .then(async (res) => {
+          let response = '';
+          for await (const chunk of res) {
+            response += chunk.choices[0]?.delta?.content || '';
+            observer.next(chunk.choices[0]?.delta?.content || '');
+          }
+          observer.complete();
+          await this.db.chat.update({
+            where: { id: chat.id },
+            data: {
+              histories: {
+                create: [
+                  {
+                    role: 'assistant',
+                    content: response,
+                  },
+                ],
+              },
+            },
+          });
+        })
+        .catch((err) => {
+          observer.error(err);
+        });
+    });
+  }
+
+  async startNewChat(userName: string, prompt: string) {
+    if (!userName) {
+      userName = 'string';
+    }
+    const user = await this.db.user.findFirst({
+      where: { name: userName },
+    });
+    if (!user) {
+      throw new UnauthorizedException('User not found');
+    }
+
+    const newChat = await this.db.chat.create({
+      data: {
+        author: { connect: { id: user.id } },
+        histories: {
+          create: [
+            {
+              role: 'user',
+              content: prompt,
+            },
+          ],
+        },
+      },
+      include: {
+        histories: true,
+      },
+    });
+
+    const historyMessages = newChat.histories.map((history) => ({
+      role: history.role as 'user' | 'assistant',
+      content: history.content,
+    }));
+
+    return new Observable((observer) => {
+      this.openai.chat.completions
+        .create({
+          model: 'solar-pro2',
+          messages: historyMessages,
+          stream: true,
+        })
+        .then(async (res) => {
+          let response = '';
+          for await (const chunk of res) {
+            response += chunk.choices[0]?.delta?.content || '';
+            observer.next(chunk.choices[0]?.delta?.content || '');
+          }
+          observer.complete();
+          await this.db.chat.update({
+            where: { id: newChat.id },
+            data: {
+              histories: {
+                create: [
+                  {
+                    role: 'assistant',
+                    content: response,
+                  },
+                ],
+              },
+            },
+          });
+        })
+        .catch((err) => {
+          observer.error(err);
+        });
+    });
+  }
+
+  async getChattings(userName: string) {
+    if (!userName) {
+      userName = 'string';
+    }
+    const user = await this.db.user.findFirst({
+      where: { name: userName },
+    });
+    if (!user) {
+      throw new UnauthorizedException('User not found');
+    }
+
+    return await this.db.chat.findMany({
+      where: { authorId: user.id },
+      include: { histories: true },
+    });
   }
 }
